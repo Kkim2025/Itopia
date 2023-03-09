d476b5aa96e677ddb78fabbcddd623befa7e803a
kayceekim <pencilkayc123@gmail.com>
2023-03-08 19:34:41 -0700

    kc

---
+++ app/src/main/java/Kaycee/Kim/itopia/List.java
@@ -0,0 +1,24 @@
+package Kaycee.Kim.itopia;
+
+public class List {
+    String num;
+    String name;
+
+    public List(String num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+    public String getNum() {
+        return num;
+    }
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/Kaycee/Kim/itopia/Store.java
+++ app/src/main/java/Kaycee/Kim/itopia/Store.java
@@ -1,4 +1,87 @@
 package Kaycee.Kim.itopia;
 
-public class Store {
+import android.content.ClipData;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Store { public class Page1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store);
+
+        ListView listview = findViewById(R.id.listView);
+        ListViewAdapter adapter = new ListViewAdapter();
+
+        //Adapter 안에 아이템의 정보 담기
+        adapter.addItem(new ClipData.Item("1", "item1"));
+        adapter.addItem(new ClipData.Item("2", "item2"));
+        adapter.addItem(new ClipData.Item("3", "item3"));
+
+        //리스트뷰에 Adapter 설정
+        listview.setAdapter(adapter);
+    }
+
+    /* 리스트뷰 어댑터 */
+    public class ListViewAdapter extends BaseAdapter {
+        ArrayList<ClipData.Item> items = new ArrayList<ClipData.Item>();
+
+        public void addItem(ClipData.Item item) {
+            items.add(item);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup viewGroup) {
+            final Context context = viewGroup.getContext();
+            final ClipData.Item item = items.get(position);
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            // listitem layout을 inflate 해준다.(memory에 올려준다)
+            convertView = inflater.inflate(R.layout.listitem_layout, viewGroup, false);
+
+            TextView tv_num = (TextView) convertView.findViewById(R.id.tv_num);
+            TextView tv_name = (TextView) convertView.findViewById(R.id.tv_name);
+
+            tv_num.setText(item.getNum());
+            tv_name.setText(item.getName());
+
+            //각 아이템 선택 event
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, item.getNum()+" "+item.getName()+" clicked!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            return convertView;  //뷰 객체 반환
+        }
+    }
 }
