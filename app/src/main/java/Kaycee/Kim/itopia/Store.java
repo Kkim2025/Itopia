530ee31ce29866896ac1a396ff9eace45b0775c4
kayceekim <pencilkayc123@gmail.com>
2023-03-29 20:52:50 -0600

    kc

--- app/src/main/java/Kaycee/Kim/itopia/Store.java
+++ app/src/main/java/Kaycee/Kim/itopia/Store.java
@@ -21,7 +21,7 @@ public class Store { public class Page1 extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.store);
+        setContentView(R.layout.list);
 
         ListView listview = findViewById(R.id.listView);
         ListViewAdapter adapter = new ListViewAdapter();
